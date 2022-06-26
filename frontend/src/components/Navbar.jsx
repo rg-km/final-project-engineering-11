0a2ca5f1123b4b1e47cfacec28a7054c36a1f062
Meilika <meilika.dwi101619@students.unila.ac.id>
2022-06-26 13:15:34 +0700

    style: findMentor

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import logoImg from '../assets/logohi.png'
 import {MenuIcon, XIcon} from '@heroicons/react/outline'
 import { Link, useNavigate } from 'react-router-dom';
